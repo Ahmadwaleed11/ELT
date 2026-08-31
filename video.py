fad2510209c81e0b84dc92f9a063e29eaa0c12d1
Ahmadwaleed11 <worldharry9@gmail.com>
2026-08-31 21:30:59 +0500

    get videoId of 50 videos

--- video.py
+++ video.py
@@ -7,7 +7,7 @@ API_KEY=os.getenv('API_KEY')
 
 
 CHANNEL_HANDLE='WildlensbyAbrar'
-
+max_result=50
 def channel_playlist_id():
     try:
         url=f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
@@ -24,8 +24,46 @@ def channel_playlist_id():
         return content
     except requests.exceptions.RequestException as e: 
         raise e 
+
+
+
+
+def get_video_id(playlist_id):
+    video_ids=[]
+    pageToken=None
+    base_url=f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_result}&playlistId={playlist_id}&key={API_KEY}'
+
+
+    try:
+        while True:
+            url=base_url
+            if pageToken:
+                url+=f'&pageToken={pageToken}'
+
+            response=requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+            for item in data.get('items',[]):
+                video_id=item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken=data.get('nextPageToken')
+
+            if not pageToken:
+                break    
+        return video_ids 
+    except requests.exceptions.RequestException as e:
+        raise e 
+    
+
+
+
+
+
 if __name__=='__main__':
-    channel_playlist_id()
-    # print('channel id is executed')
+    # playlist_id=channel_playlist_id()
+    # print(get_video_id(playlist_id))
+    
+    print('channel id is executed')
 else:
     print('channel id is not executed')
